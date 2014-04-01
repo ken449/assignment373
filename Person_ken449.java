4b42a274dc867c553dd51d436e3f80d12c98047e
ken449 <ken449.test@gmail.com>
2014-04-01 10:32:40 -0500

    Update Person_ken449.java

--- Person_ken449.java
+++ Person_ken449.java
@@ -2,4 +2,9 @@ public class Person_ken449 {
 //Write a Person class in Java that has a constructor
 //Name your Person class Person_<<YOUREID>>.java
 
+    // added constructor.
+    public Person_ken449() {
+      
+    
+    }
 }
